483153026a48b3905bc9e98a1b033f53d8d1cc47
AmusedDiffuser <81667911+AmusedDiffuser@users.noreply.github.com>
2023-06-04 20:13:16 -0400

    Update operators.py

--- operators.py
+++ operators.py
@@ -1,179 +1,136 @@
 # Import the necessary modules
 import bpy
+import logging
 import os
 
-# Define some helper functions for the addon logic
+# Import the mathutils module for math operations
+from mathutils import Vector
 
-def create_material(name):
-    """Create a new material with a given name.
+# Import the imbuf module for image processing
+from imbuf import load as imbuf_load
 
-    Args:
-        name (str): The name of the material.
+# Import the translation function
+from bpy.app.translations import pgettext_iface as iface_
 
-    Returns:
-        bpy.types.Material: The material that was created.
-    """
-    # Create a new material using bpy.data.materials.new()
-    material = bpy.data.materials.new(name)
+# Define a custom operator class for creating a surface object from an image file
+class DepthifyCreateSurfaceOperator(bpy.types.Operator):
+    """Create a 3D surface from a depth map image"""
 
-    # Enable nodes for the material using material.use_nodes = True
-    material.use_nodes = True
+    # Define some metadata for the operator
+    bl_idname = "object.depthify_create_surface"
+    bl_label = iface_("Create Surface")
+    bl_options = {'REGISTER', 'UNDO'}
 
-    # Return the material
-    return material
+    # Define a function to execute the operator
+    def execute(self, context):
+        # Get the current scene and its properties
+        scene = context.scene
+        props = scene.depthify_properties
 
+        # Get the image file path from the image property
+        image_file = props.image
 
-def create_image_texture_node(material, image_path, color_space):
-    """Create an image texture node for a material with a given image path and color space.
+        # Validate the image file path
+        if not image_file:
+            # Log an error message to the console and the UI
+            logging.error("No image file selected")
+            self.report({'ERROR'}, "No image file selected")
+            return {'CANCELLED'}
 
-    Args:
-        material (bpy.types.Material): The material to create the node for.
-        image_path (str): The path of the image file to load.
-        color_space (str): The color space of the image ('COLOR' or 'NONE').
+        # Get the absolute path of the image file
+        try:
+            image_file = bpy.path.abspath(image_file)
+        except Exception as e:
+            # Log an error message to the console and the UI
+            logging.error(f"Invalid image file path: {e}")
+            self.report({'ERROR'}, f"Invalid image file path: {e}")
+            return {'CANCELLED'}
 
-    Returns:
-        bpy.types.ShaderNodeTexImage: The image texture node that was created.
-    """
-    # Get the node tree of the material using material.node_tree
-    node_tree = material.node_tree
+        # Check if the image file exists and is readable
+        if not os.path.exists(image_file) or not os.access(image_file, os.R_OK):
+            # Log an error message to the console and the UI
+            logging.error(f"Image file not found or not readable: {image_file}")
+            self.report({'ERROR'}, f"Image file not found or not readable: {image_file}")
+            return {'CANCELLED'}
 
-    # Get the nodes and links of the node tree using node_tree.nodes and node_tree.links
-    nodes = node_tree.nodes
-    links = node_tree.links
+        # Load the image file using imbuf module
+        try:
+            image = imbuf_load(image_file)
+        except Exception as e:
+            # Log an error message to the console and the UI
+            logging.error(f"Failed to load image file: {e}")
+            self.report({'ERROR'}, f"Failed to load image file: {e}")
+            return {'CANCELLED'}
 
-    # Create a new image texture node using nodes.new('ShaderNodeTexImage')
-    image_node = nodes.new('ShaderNodeTexImage')
+        # Get the width and height of the image
+        width = image.width
+        height = image.height
 
-    # Load the image from the image path using bpy.data.images.load()
-    image = bpy.data.images.load(image_path)
+        # Get the depth map values from the image pixels
+        depth_map = [pixel[0] for pixel in image.pixels]
 
-    # Set the color space of the image using image.colorspace_settings.name
-    image.colorspace_settings.name = color_space
+        # Store the depth map values in the depth map property
+        props.depth_map.clear()
+        props.depth_map.extend(depth_map)
 
-    # Assign the image to the image texture node using image_node.image = image
-    image_node.image = image
+        # Create a surface object using depthify module
+        try:
+            surface = depthify.create_surface(width, height, depth_map)
+        except Exception as e:
+            # Log an error message to the console and the UI
+            logging.error(f"Failed to create surface object: {e}")
+            self.report({'ERROR'}, f"Failed to create surface object: {e}")
+            return {'CANCELLED'}
 
-    # Return the image texture node
-    return image_node
+        # Store the surface object in the surface property
+        props.surface = surface
 
+        # Apply adaptive subdivision to the surface object using depthify module
+        try:
+            depthify.apply_adaptive_subdivision(surface, props.subdivisions, props.subdivision_type)
+        except Exception as e:
+            # Log an error message to the console and the UI
+            logging.error(f"Failed to apply adaptive subdivision: {e}")
+            self.report({'ERROR'}, f"Failed to apply adaptive subdivision: {e}")
+            return {'CANCELLED'}
 
-def create_displacement_node(material):
-    """Create a displacement node for a material.
+        # Apply displacement to the surface object using depthify module
+        try:
+            depthify.apply_displacement(surface, props.displacement_strength, props.displacement_type)
+        except Exception as e:
+            # Log an error message to the console and the UI
+            logging.error(f"Failed to apply displacement: {e}")
+            self.report({'ERROR'}, f"Failed to apply displacement: {e}")
+            return {'CANCELLED'}
 
-    Args:
-        material (bpy.types.Material): The material to create the node for.
+        # Scale the surface object using depthify module
+        try:
+            depthify.scale_surface(surface, props.scale)
+        except Exception as e:
+            # Log an error message to the console and the UI
+            logging.error(f"Failed to scale surface object: {e}")
+            self.report({'ERROR'}, f"Failed to scale surface object: {e}")
+            return {'CANCELLED'}
 
-    Returns:
-        bpy.types.ShaderNodeDisplacement: The displacement node that was created.
-    """
-    # Get the node tree of the material using material.node_tree
-    node_tree = material.node_tree
+        # Link the surface object to the scene
+        scene.collection.objects.link(surface)
 
-    # Get the nodes and links of the node tree using node_tree.nodes and node_tree.links
-    nodes = node_tree.nodes
-    links = node_tree.links
+        # Set the surface object as the active object
+        scene.view_layers[0].objects.active = surface
 
-    # Create a new displacement node using nodes.new('ShaderNodeDisplacement')
-    displacement_node = nodes.new('ShaderNodeDisplacement')
+        # Log a success message to the console and the UI
+        logging.info(f"Surface object created from image file: {image_file}")
+        self.report({'INFO'}, f"Surface object created from image file: {image_file}")
 
-    # Return the displacement node
-    return displacement_node
+        # Return a success status
+        return {'FINISHED'}
 
+# Define a function to register the operator class
+def register():
+    # Register the operator class
+    bpy.utils.register_class(DepthifyCreateSurfaceOperator)
 
-def create_principled_node(material):
-    """Create a principled BSDF node for a material.
-
-    Args:
-        material (bpy.types.Material): The material to create the node for.
-
-    Returns:
-        bpy.types.ShaderNodeBsdfPrincipled: The principled BSDF node that was created.
-    """
-    
-     # Get the node tree of the material using material.node_tree
-     node_tree = material.node_tree
-
-     # Get the nodes and links of the node tree using node_tree.nodes and node_tree.links
-     nodes = node_tree.nodes
-     links = node_tree.links
-
-     # Create a new principled BSDF node using nodes.new('ShaderNodeBsdfPrincipled')
-     principled_node = nodes.new('ShaderNodeBsdfPrincipled')
-
-     # Return the principled BSDF node
-     return principled_node
-
-
-def create_output_node(material):
-     """Create an output node for a material.
-
-     Args:
-         material (bpy.types.Material): The material to create the node for.
-
-     Returns:
-         bpy.types.ShaderNodeOutputMaterial: The output node that was created.
-     """
-     # Get the node tree of the material using material.node_tree
-     node_tree = material.node_tree
-
-     # Get the nodes and links of the node tree using node_tree.nodes and node_tree.links
-     nodes = node_tree.nodes
-     links = node_tree.links
-
-     # Create a new output node using nodes.new('ShaderNodeOutputMaterial')
-     output_node = nodes.new('ShaderNodeOutputMaterial')
-
-     # Return the output node
-     return output_node
-
-
-def create_subdivision_modifier(object, levels, render_levels):
-    """Create a subdivision modifier for an object with given levels and render levels.
-
-    Args:
-        object (bpy.types.Object): The object to create the modifier for.
-        levels (int): The number of subdivisions to apply in the viewport.
-        render_levels (int): The number of subdivisions to apply in the render.
-
-    Returns:
-        bpy.types.SubsurfModifier: The subdivision modifier that was created.
-    """
-    # Create a new subdivision modifier using object.modifiers.new()
-    modifier = object.modifiers.new(name='Subdivision', type='SUBSURF')
-
-    # Set the levels and render levels of the modifier using modifier.levels and modifier.render_levels
-    modifier.levels = levels
-    modifier.render_levels = render_levels
-
-    # Return the modifier
-    return modifier
-
-
-def create_plane_object(name, location, rotation, scale):
-    """Create a plane object with a given name, location, rotation, and scale.
-
-    Args:
-        name (str): The name of the object.
-        location (tuple): The location of the object as a 3D vector.
-        rotation (tuple): The rotation of the object as a 3D vector in radians.
-        scale (tuple): The scale of the object as a 3D vector.
-
-    Returns:
-        bpy.types.Object: The plane object that was created.
-    """
-    # Create a new plane mesh using bpy.data.meshes.new()
-    mesh = bpy.data.meshes.new(name)
-
-    # Create a new plane object using bpy.data.objects.new()
-    object = bpy.data.objects.new(name, mesh)
-
-    # Link the object to the current collection using bpy.context.collection.objects.link()
-    bpy.context.collection.objects.link(object)
-
-    # Set the location, rotation, and scale of the object using object.location, object.rotation_euler, and object.scale
-    object.location = location
-    object.rotation_euler = rotation
-    object.scale = scale
-
-    # Return the object
-    return object
+# Define a function to unregister the operator class
+def unregister():
+    # Unregister the operator class
+    bpy.utils.unregister_class(DepthifyCreateSurfaceOperator)
